1342ca693dabfdcc6849b0babf9f7026ff5cf738
ajike3 <69168968+ajike3@users.noreply.github.com>
2020-09-21 16:14:38 -0400

    changes

---
+++ assests/css/javascript/game.js
@@ -0,0 +1,8 @@
+console.log("Hello")
+let stringcountvalue
+let countvalue = Number(stringcountvalue)
+let teamOneShoot = document.querySelector("#teamone-shoot-button")
+if (x% 2 == 0) {
+    x = number(teamOneGoals.innerHTML) + 1; 
+
+}
