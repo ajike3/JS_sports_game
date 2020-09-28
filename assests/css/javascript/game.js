fc7765468af267e74848a8eafb2766899324365a
ajike3 <69168968+ajike3@users.noreply.github.com>
2020-09-27 21:47:48 -0400

    WORKING ON AUDIO

--- assests/css/javascript/game.js
+++ assests/css/javascript/game.js
@@ -1,8 +1,42 @@
-console.log("Hello")
-let stringcountvalue
-let countvalue = Number(stringcountvalue)
-let teamOneShoot = document.querySelector("#teamone-shoot-button")
-if (x% 2 == 0) {
-    x = number(teamOneGoals.innerHTML) + 1; 
+let teamOneShot = document.querySelector("#teamone-numshots");
+let teamTwoShot = document.querySelector("#teamtwo-numshots");
+let NumberOfReset = document.querySelector("#num-resets");
+let teamOneShootButton = document.querySelector("#teamone-shoot-button");
+let teamTwoShootButton = document.querySelector("#teamtwo-shoot-button");
+let resetButton = document.querySelector("#reset-button");
+let teamOneGoal = document.querySelector("#teamone-numgoals");
+let teamTwoGoal = document.querySelector("#teamtwo-numgoals");
 
+teamOneShootButton.addEventListener("click", function () {
+    playaudio(Audio);
+    console.log("1-counter")
+    let newCounterValue = Number(teamOneShot.innerHTML) + 1;
+    teamOneShot.innerHTML = newCounterValue;
+    if (Math.random() <.8) {
+      let newteam1Goal = Number(teamOneGoal.innerHTML) + 1;
+      teamOneGoal.innerHTML = newteam1Goal;
+    }
+})
+teamTwoShootButton.addEventListener("click", function () {
+    playaudio()
+    console.log("2-counter");
+    let newCounterValue = Number(teamTwoShot.innerHTML) + 1;
+    teamTwoShot.innerHTML = newCounterValue;
+    if (Math.random() < 0.4) {
+      let newteam2Goal = Number(teamTwoGoal.innerHTML) + 1;
+      teamTwoGoal.innerHTML = newteam2Goal;
+    }
+})
+resetButton.addEventListener("click", function () {
+        newFunction();
+    let newCounterValue = Number(NumberOfReset.innerHTML) + 1;
+    NumberOfReset.innerHTML = newCounterValue;
+        teamTwoShot.innerHTML = 0;
+        teamOneShot.innerHTML = 0;
+        teamTwoGoal.innerHTML = 0;
+        teamOneGoal.innerHTML = 0;
+})
+
+function newFunction() {
+    console.log("reset-counter");
 }
